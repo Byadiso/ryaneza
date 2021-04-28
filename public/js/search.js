9918810fa4f1aca4d8c558c33de390fac4c7eb93
Byadiso <byadiso@gmail.com>
2021-04-28 18:28:14 +0200

    adding category id from storage

--- public/js/search.js
+++ public/js/search.js
@@ -7,39 +7,76 @@ document.addEventListener('DOMContentLoaded', () => {
     const submit_serch_btn = document.querySelector('.btnSearch');
     const search_container = document.querySelector('.search_container');
     let searchedContent = document.querySelector('#searched_content');
-    const main_properties = document.querySelector('.main_properties');
+    const main_properties = document.querySelector('.main_properties');  
+    const filters = document.querySelector('.filterCheck');
+
+
+    let categoryAny = "";
+
 
 // fetching of categories 
-// function fetchCategories(){
-//     return  fetch(`http://localhost:3000/api/v1/categories`, {
-//              method: 'GET',
-//              headers:{
-//                'Content-Type':'application/json'
-//                     }
+        function fetchCategories(){
+        return  fetch(`http://localhost:3000/api/v1/categories`, {
+                method: 'GET',
+                headers:{
+                'Content-Type':'application/json'
+                        }
+        })
+        .then(response =>response.json())
+        .then(categories =>{ 
+        let storedCategories = localStorage.setItem('categories', JSON.stringify(categories))
 
-//        })
-//      .then(response =>response.json())
-//      .then(categories =>{ 
-//       let storedCategories = localStorage.setItem('categories', JSON.stringify(categories))
-   
-//      })
-//      .catch(err =>console.log(err));
-//    };
+        })
+        .catch(err =>console.log(err));
+        };
 
-//    fetchCategories();
+        fetchCategories();
 
 
-//    get them from localStorage
-    let CategoriesStored = JSON.parse(localStorage.getItem('categories'));
-    console.log(CategoriesStored);
-    // let categoryAny= "fresh vegetables"
-    // let categoryAny = "6076d05f3d6773321c50cb9a"
-    let categoryAny = ""
+    //   get them from localStorage
+let CategoriesStored = JSON.parse(localStorage.getItem('categories'));
+console.log(CategoriesStored);
+
+
+function renderCategory(){      
+   CategoriesStored.forEach(category => {
+    const listOfCategories = document.createElement('div');
+    listOfCategories.classList.add('check_item');
+    listOfCategories.innerHTML = `                 
+            <input type="checkbox" name="${category.name}" id="all_property" value="${category.name}">
+            <label for="all"><strong>${category.name}</strong></label>                
+   `;
+
+    filters.appendChild(listOfCategories);
+    });         
+}
+
+
+renderCategory();
+
+
+// let handle change on checking event 
+
+filters.addEventListener('change',(e)=>{
+ let filter = e.target.value;
+    // console.log(filter);
+    let filtredCatId = CategoriesStored.find(cat => cat.name === filter);
+    // filter ? categoryAny = filtredCatId._id : categoryAny = "" ; 
+    categoryAny = filtredCatId._id;   
+    console.log(categoryAny); 
+    // find the id of the category     
+})
+
+// le
+// let filtredCatId = CategoriesStored.find(cat => cat.name === filter);
+//     console.log(filtredCatId)
+
+
+// //    get id  from localStorage for category
+//     
+    
    ///handle search business 
 
-    // const handleChange = name => event => {
-    //  name = event.target.value;
-    // };
 
     let input_search
     const input = document.getElementById('input_search');
@@ -77,7 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let search = input_search;
         // console.log(search, category);
         if (search) {
-            list({ search: input_search || undefined, category: categoryAny }).then(
+            list({ search: input_search || undefined, category: categoryAny  }).then(
                 response => {
                     if (response.error) {
                         console.log(response.error);
