b5684bb419dd84780161f9f093922018dc309189
Byadiso <byadiso@gmail.com>
2021-05-03 14:31:40 +0200

    finishing displaying comments

--- public/js/singlePro.js
+++ public/js/singlePro.js
@@ -1,191 +1,157 @@
-
 /* eslint-disable prettier/prettier */
-document.addEventListener('DOMContentLoaded', ()=>{ 
-            
-  // const ordersMsg = document.getElementById('header-text');  
-  const mainSingleDiv = document.getElementById('singleProperty'); 
-  let proId = localStorage.getItem('id');
-  let propertiesItem = {...JSON.parse(localStorage.getItem('properties'))};
+document.addEventListener('DOMContentLoaded', () => {
+    // const ordersMsg = document.getElementById('header-text');
+    const mainSingleDiv = document.getElementById('singleProperty')
+    let proId = localStorage.getItem('id')
+    let propertiesItem = { ...JSON.parse(localStorage.getItem('properties')) }
 
-  // console.log(properties);
-  
-  console.log(proId);
-  let pro =[];
-  pro = [...pro, propertiesItem];
-  
-  let Mypro = pro.find(item => ()=>{
-     item.Property.id[0]=== proId }
-  )
+    // console.log(properties);
 
-  let newPro = Mypro.properties 
-  console.log(newPro);
-  let findedOne = newPro.find(item=> item._id === proId)
-    
-  console.log(findedOne);
-  let {description,shipping,reviews,comments,_id} = findedOne
+    console.log(proId)
+    let pro = []
+    pro = [...pro, propertiesItem]
 
-  //for acessing my comments reviews and description
-  const descriptionCont = document.querySelector('.description_details');
-  const ShippingCont = document.querySelector('.shipping_details');
-  const reviewsCont = document.querySelector('.reviews_details');
-  const commentsCont = document.querySelector('#comment_details_container');
-  
-  
+    let Mypro = pro.find((item) => () => {
+        item.Property.id[0] === proId
+    })
 
-  // set all content 
-  descriptionCont.textContent= description;
-  ShippingCont.textContent= shipping == true ? "Please remember to update your address in order to deliver you product at your door" : "Sorry this is not delivable product";
-  reviewsCont.textContent=reviews.length == 0 ? "No Reviews found!" : reviews.map((review)=>{
-    const reviewsContainer = document.createElement('div');
-    reviewsContainer.innerHTML =
-    ` <p>${review}</p>
+    let newPro = Mypro.properties
+    console.log(newPro)
+    let findedOne = newPro.find((item) => item._id === proId)
+
+    console.log(findedOne)
+    let { description, shipping, reviews, comments, _id } = findedOne
+
+    //for acessing my comments reviews and description
+    const descriptionCont = document.querySelector('.description_details')
+    const ShippingCont = document.querySelector('.shipping_details')
+    const reviewsCont = document.querySelector('.reviews_details')
+    const commentsCont = document.querySelector('#comment_details_container')
+
+    // set all content
+    descriptionCont.textContent = description
+    ShippingCont.textContent =
+        shipping == true
+            ? 'Please remember to update your address in order to deliver you product at your door'
+            : 'Sorry this is not delivable product'
+    reviewsCont.textContent =
+        reviews.length == 0
+            ? 'No Reviews found!'
+            : reviews.map((review) => {
+                  const reviewsContainer = document.createElement('div')
+                  reviewsContainer.innerHTML = ` <p>${review}</p>
     <p>${review.postedBy.name}</p>
     `
-  });
+              })
 
-  // ...........................comment section......................................................
-  const comment_details_header = document.querySelector('.comment_details_header');
-  comment_details_header.textContent = comments.length; 
+    // ...........................comment section......................................................
+    const comment_details_header = document.querySelector(
+        '.comment_details_header'
+    )
+    comment_details_header.textContent = comments.length
 
-  commentsCont.innerHTML = comments.length == 0 ? "No comments found for this product. Be the first to comment!" : renderComments();
+    // condition to for rendering a text if no comments and if there are comments show them
 
-  function renderComments(){
-    
-    for(var i= 0; i< comments.length; i++){
-     
-      const {text , _id, createdBy, created } = comments[i];
+    if (comments.length == 0) {
+        return 'No comments to dislay'
+    } else {
+        renderComments()
+    }
 
-      // console.log(`comment: ${_id}${text}, Created on ${created}, by : ${createdBy}`);
-      
-      const comentContainer = document.createElement('div');
-      comentContainer.innerHTML =
-        ` <p>${_id}</p>
+    // render my comments
+    function renderComments() {
+        for (var i = 0; i < comments.length; i++) {
+            const { text, _id, createdBy, created } = comments[i]
+            const comentContainer = document.createElement('div')
+            comentContainer.innerHTML = ` <p>${_id}</p>
            <p>${text}</p>
            <p>${createdBy}</p>        
-           <p>${created}</p>`;
+           <p>${created}</p>
+           <hr />`
 
-        commentsCont.append(comentContainer);
-
+            commentsCont.append(comentContainer)
+        }
     }
-  }
-  
+// ..................................render property ................................................
 
-         
- const renderPro = ()=>{  
-   const singlePro = findedOne;
-   const propertyContainer= document.createElement('DIV');
-   propertyContainer.classList.add('property_container');
-   propertyContainer.setAttribute('data-id',_id);
-  
+    const renderPro = () => {
+        const singlePro = findedOne
+        const propertyContainer = document.createElement('DIV')
+        propertyContainer.classList.add('property_container')
+        propertyContainer.setAttribute('data-id', _id)
 
-          //for add to the cart
-          const addCartBtn= document.createElement('BUTTON');
-          addCartBtn.classList.add('btn_addCart');
-          addCartBtn.classList.add('bag-btn');
-          addCartBtn.innerHTML = `<i class="fas fa-shopping-cart"></i> Add to cart`;
-          addCartBtn.style.margin = "5px 2px 5px 2px";
-          addCartBtn.addEventListener('click', ()=>{
-                console.log('yes this one is already added');
-                location.href='#';
+        //for add to the cart
+        const addCartBtn = document.createElement('BUTTON')
+        addCartBtn.classList.add('btn_addCart')
+        addCartBtn.classList.add('bag-btn')
+        addCartBtn.innerHTML = `<i class="fas fa-shopping-cart"></i> Add to cart`
+        addCartBtn.style.margin = '5px 2px 5px 2px'
+        addCartBtn.addEventListener('click', () => {
+            console.log('yes this one is already added')
+            location.href = '#'
+        })
 
-          });
-          
-
-          
-
-
-    //for image
-    let photoUrl = `http://localhost:3000/api/v1/property/photo/${proId}`
-        const img = document.createElement("img");  
-        img.src = photoUrl; 
-        img.style.width= "350px";
-        img.style.height= "400px";
-        img.classList.add('imgCreated');
-        propertyContainer.append(img);
-      //   propertyContainer.append(deleteBtn);
-      //   propertyContainer.append(modifyBtn);
-        propertyContainer.append(addCartBtn);
+        //for image
+        let photoUrl = `http://localhost:3000/api/v1/property/photo/${proId}`
+        const img = document.createElement('img')
+        img.src = photoUrl
+        img.style.width = '350px'
+        img.style.height = '400px'
+        img.classList.add('imgCreated')
+        propertyContainer.append(img)
+        //   propertyContainer.append(deleteBtn);
+        //   propertyContainer.append(modifyBtn);
+        propertyContainer.append(addCartBtn)
         //appending the main container
-        mainSingleDiv.appendChild(propertyContainer);
-        
-        
-              
- }
+        mainSingleDiv.appendChild(propertyContainer)
+    }
 
-//  fetchingSingle();
+    //  fetchingSingle();
 
+    renderPro()
 
+    // ----------------------------------------------------------------------------------
 
-renderPro();
+    //fetching related
+    const fetchingRelated = () => {
+        fetch(`http://localhost:3000/api/v1/properties/related/${proId}/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => renderRelated(data))
+    }
 
+    function renderRelated(data) {
+        console.log(data)
 
-// ----------------------------------------------------------------------------------
+        let properties = data
+        console.log(properties)
+        const container_related = document.createElement('DIV')
+        container_related.classList.add('property_container')
+        let header_related = document.createElement('h1')
+        header_related.classList.add('header_related')
+        header_related.textContent = 'Related Property'
 
+        // create a header for related property
+        container_related.append(header_related)
 
+        for (var i = 0; i < properties.length; i++) {
+            const { _id, name, category } = properties[i]
+            let property_related = document.createElement('div')
+            property_related.classList.add('related_properties')
 
-//fetching related 
-const  fetchingRelated = ( () => {
-      fetch( `http://localhost:3000/api/v1/properties/related/${proId}/`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-       
-        },
-      }
-        )
-      .then((res) => res.json())
-      .then(data => renderRelated(data)
-         )    
-   }); 
-
-function renderRelated(data){
-      console.log(data);
-      
-      let properties = data;
-      console.log(properties);
-      const container_related = document.createElement('DIV');
-      container_related.classList.add('property_container');
-      let header_related = document.createElement('h1');
-      header_related.classList.add('header_related');
-      header_related.textContent= "Related Property";
-
-      // create a header for related property
-      container_related.append(header_related);
-
-
-
-
-      for ( var i= 0; i < properties.length; i++ ){ 
-    
-            const {_id,name, category} = properties[i];
-            let property_related = document.createElement('div');
-            property_related.classList.add('related_properties');
-      
-        
-            property_related.innerHTML = 
-         `<img scr=http://localhost:3000/api/v1/property/photo/${_id} class="imgCreated" style="width: 100px; height: 50px;">
+            property_related.innerHTML = `<img scr=http://localhost:3000/api/v1/property/photo/${_id} class="imgCreated" style="width: 100px; height: 50px;">
          <p id="phone"><strong>name:</strong> ${name}</p>        
           `
-      
-          container_related.append(property_related); 
-          
-          mainSingleDiv.appendChild(container_related); 
 
-          
+            container_related.append(property_related)
 
+            mainSingleDiv.appendChild(container_related)
+        }
+    }
 
-
-      }
-
-     
-
-
-}
-
-   fetchingRelated();
-
-
+    fetchingRelated()
 })
-
-
