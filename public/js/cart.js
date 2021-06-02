1d589b81d23aa09c68418487f5ceb42546e126bf
Byadiso <byadiso@gmail.com>
2021-06-02 02:12:56 +0200

    cart logic

--- public/js/cart.js
+++ public/js/cart.js
@@ -2,13 +2,67 @@
 document.addEventListener('DOMContentLoaded', () => {
    
     console.log('Welcome to the cart logic')
-    var addToCartButton = document.getElementsByClassName(".btn_addCart");
+     
+
+    document.body.addEventListener( 'click', function ( event ) {           
+            if( event.target && event.target.matches(".btn_addCart")) {            
+                let item_id = event.target.parentNode.dataset.id;
+                let buttonAddToCart =  event.target;                     
+                addToCart(item_id, buttonAddToCart ); 
+                // checkIfInCart(item_id) 
+                     
+            };
+          } );
 
 
-    document.body.addEventListener( 'click', function ( event ) {
+    function addToCart(item_id, buttonContent){ 
+          let cart=[];         
         
-        if( event.target.classname === '.btn_addCart' ) {
-          console.log("yes")
-        };
-      } );
+
+          let propertiesItem = { ...JSON.parse(localStorage.getItem('properties')) } ;          
+            let itemTobeAdded =  propertiesItem.properties.find((item) => () => {
+            item.Property.id[0] === item_id
+        });
+            
+            console.log("let add to the cart")
+            cart =[...cart, itemTobeAdded];    
+            localStorage.setItem('cart', JSON.stringify(cart));             
+            buttonContent.innerHTML=`<i class="fas fa-shopping-cart"></i> Added to cart`
+            console.log(cart)
+            
+        }
+        
+  
+    //   function checkIfInCart(id){
+          
+    // let propertiesItem = { ...JSON.parse(localStorage.getItem('properties')) } ;          
+    // let itemTobeAdded =  propertiesItem.properties.find((item) => () => {
+    //         item.Property.id[0] === id
+    //     });
+
+    // let cartStored = [...JSON.parse(localStorage.getItem('cart')) ]  
+    // console.log(cartStored)      
+    // let isInCart = cartStored.find((item) => () => {
+    //     item._id === itemTobeAdded._id
+    // }); 
+
+    // if(isInCart){
+    //     return "it is in cart"
+    // }
+    // else{
+    //     return "it is not in cart"
+    // }
+
+    //   }
+
+    function displayCart(){
+        let cartContiner = document.getElementsByClassName('cart-content');
+
+        let cartStored = [...JSON.parse(localStorage.getItem('cart')) ] 
+        cartStored.forEach(item =>{
+            console.log(item)
+        })    
+    }
+
+    displayCart();
 })
