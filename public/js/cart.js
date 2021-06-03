da8c51be4305dca4599aaedceb3f811d21e33c4c
Byadiso <byadiso@gmail.com>
2021-06-03 16:42:22 +0200

    fixing bug with cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,77 +1,52 @@
 /* eslint-disable prettier/prettier */
 document.addEventListener('DOMContentLoaded', () => {
    
-    let totalDisplay = document.querySelector('.cart-total');    
-
-    // document.body.addEventListener( 'click', function ( event ) {           
-    //         if( event.target && event.target.matches(".btn_addCart")) {            
-    //             let item_id = event.target.parentNode.dataset.id;
-    //             let buttonAddToCart =  event.target;                     
-    //             addToCart(item_id, buttonAddToCart ); 
-    //             // checkIfInCart(item_id) 
-                     
-    //         };
-    //       } );
-
-
-    // function addToCart(item_id, buttonContent){ 
-    //         let cart=[];     
-    //         let propertiesItem = { ...JSON.parse(localStorage.getItem('properties')) } ;          
-    //             let itemTobeAdded =  propertiesItem.properties.find((item) => () => {
-    //             item.Property.id === item_id
-    //         });            
-    //         console.log("let add to the cart");            
-    //         cart.push({
-    //             ...itemTobeAdded,
-    //             count: 1
-    //         });   
-    //         localStorage.setItem('cart', JSON.stringify(cart));             
-    //         buttonContent.innerHTML=`<i class="fas fa-shopping-cart"></i>In cart`
-    //         console.log(cart);  
-            
-    //     }
-        
+    let totalDisplay = document.querySelector('.cart-total');  
+    let cartContainer = document.querySelector('.cart-content');     
           
     function displayCart(){
         if (typeof window !== 'undefined') {
-            if (localStorage.getItem('cart')) {            
-               
-                let cartContiner = document.querySelector('.cart-content');
+            if (localStorage.getItem('cart')) {     
+                
                 let cartStored = [...JSON.parse(localStorage.getItem('cart')) ];
-                let productInCart = document.createElement('DIV');
-                cartStored.forEach(item =>{
-                    let {_id, name, price, count } = item
+                let elements = document.createElement('DIV');
+                for ( var i= 0; i < cartStored.length; i++ ){                    
+                    let {_id, name, price, count } = cartStored[i];
+                    console.log(cartStored[i])
+                    
                     let photoUrl = `http://localhost:3000/api/v1/property/photo/${_id}` ;
                     
-                    productInCart.innerHTML=`
-                    <div class="product_in_cart_container" data-id="${_id}">
-                        <div class="product_in_cart_image">
-                            <img src=${photoUrl} class="imgCreated" style="width: 50px; height: 50px;">
-                        </div>
-                        <div class="product_in_cart_name">
-                            <span>${name}</span>
-                        </div>
-                        <div class="product_in_cart_price">
-                        <span>${price}</span>
-                        </div>
-                    
-                    </div>
-                    `
-                cartContiner.appendChild(productInCart);
-                calculatePrice(price,count);
-                })
+                    elements.innerHTML= `
+                            <div class="product_in_cart_container" data-id="${_id}">
+                                <div class="product_in_cart_image">
+                                    <img src=${photoUrl} class="imgCreated" style="width: 50px; height: 50px;">
+                                </div>
+                                <div class="product_in_cart_name">
+                                    <span>${name}</span>
+                                </div>
+                                <div class="product_in_cart_price">
+                                    <span>${price}</span>
+                                </div>                            
+                            </div>
+                        `
+                    cartContainer.appendChild(elements);    
+                    calculatePrice(price,count);
+                }
+              
             }
         };
         
-        //for calculation
-        function calculatePrice(price,quantity){
-            let total = 0
-            let product= parseInt(quantity*price);
-            total += product
-            console.log(total)
-            totalDisplay.textContent = total
-        }
+       
     }
 
     displayCart();
+
+
+     //for calculation
+     function calculatePrice(price,quantity){
+        let total = 0
+        let product= parseInt(quantity*price);
+        total += product        
+        totalDisplay.textContent = total
+    }
 })
